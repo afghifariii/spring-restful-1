232fc03c3a845e975e2583c0ebaff47c2c98cf76
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-18 09:12:04 +0700

    Initial commit

---
+++ src/main/java/com/spring/restful/first/SpringRestfulFirstApplication.java
@@ -0,0 +1,12 @@
+package com.spring.restful.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulFirstApplication.class, args);
+	}
+}
